efc1e14e229d46a1e9e4151c79009cc662de4900
Chanwut Kittivorawong <chanwutk@gmail.com>
2018-11-05 22:25:26 -0800

    FillBitMap

--- src/FillBitMap.js
+++ src/FillBitMap.js
@@ -4,15 +4,36 @@
 import BitMap from './BitMap';
 import { Marks } from 'vega-scenegraph';
 
+// bit mask for getting first 2 bytes of alpha value
 var ALPHA_MASK = 0xff000000;
-var INSIDE_OPACITY_IN_ALPHA = 0x10000000; // opacity at 0.0625 in alpha
+
+// alpha value equivalent to opacity 0.0625
+var INSIDE_OPACITY_IN_ALPHA = 0x10000000;
 var INSIDE_OPACITY = 0.0625;
 
+/**
+ * Get bitmaps and fill the with mark information from data
+ * @param {array} data data of labels to be placed
+ * @param {array} size size of chart in format [width, height]
+ * @param {string} marktype marktype of the base mark
+ * @param {bool} avoidBaseMark a flag if base mark is to be avoided
+ * @param {array} avoidMarks array of mark data to be avoided
+ * @param {bool} labelInside a flag if label to be placed inside mark or not
+ * @param {number} padding padding from the boundary of the chart
+ *
+ * @returns array of 2 bitmaps:
+ *          - first bitmap is filled with all the avoiding marks
+ *          - second bitmap is filled with borders of all the avoiding marks (second bit map can be
+ *            undefined if checking border of base mark is not needed when not avoiding any mark)
+ */
 export default function(data, size, marktype, avoidBaseMark, avoidMarks, labelInside, padding) {
   var isGroupArea = marktype === 'group' && data[0].datum.datum.items[0].marktype === 'area',
     width = size[0],
     height = size[1],
     n = data.length;
+
+  // extract data information from base mark when base mark is to be avoid
+  // or base mark is implicitly avoid when base mark is group area
   if (marktype && (avoidBaseMark || isGroupArea)) {
     var items = new Array(n);
     for (var i = 0; i < n; i++) {
@@ -22,11 +43,13 @@ export default function(data, size, marktype, avoidBaseMark, avoidMarks, labelIn
   }
 
   if (avoidMarks.length) {
+    // when there is at least one mark to be avoided
     var context = writeToCanvas(avoidMarks, width, height, labelInside || isGroupArea);
     return writeToBitMaps(context, width, height, labelInside, isGroupArea, padding);
   } else {
     var bitMap = new BitMap(width, height, padding);
     if (avoidBaseMark) {
+      // when there is no base mark but data points are to be avoided
       for (i = 0; i < n; i++) {
         var d = data[i];
         bitMap.mark(d.markBound[0], d.markBound[3]);
@@ -36,6 +59,15 @@ export default function(data, size, marktype, avoidBaseMark, avoidMarks, labelIn
   }
 }
 
+/**
+ * Write marks to be avoided to canvas to be written to bitmap later
+ * @param {array} avoidMarks array of mark data to be avoided
+ * @param {number} width width of the chart
+ * @param {number} height height of the chart
+ * @param {bool} labelInside a flag if label to be placed inside mark or not
+ *
+ * @returns canvas context, to which all avoiding marks are drawn
+ */
 function writeToCanvas(avoidMarks, width, height, labelInside) {
   var m = avoidMarks.length,
     // c = document.getElementById('canvas-render'), // debugging canvas
@@ -45,6 +77,7 @@ function writeToCanvas(avoidMarks, width, height, labelInside) {
   c.setAttribute('width', width);
   c.setAttribute('height', height);
 
+  // draw every avoiding marks into canvas
   for (var i = 0; i < m; i++) {
     originalItems = avoidMarks[i];
     itemsLen = originalItems.length;
@@ -57,6 +90,19 @@ function writeToCanvas(avoidMarks, width, height, labelInside) {
   return context;
 }
 
+/**
+ * Write avoid marks from drawn canvas to bitmap
+ * @param {object} context canvas context, to which all avoiding marks are drawn
+ * @param {number} width width of the chart
+ * @param {number} height height of the chart
+ * @param {bool} labelInside a flag if label to be placed inside mark or not
+ * @param {bool} isGroupArea a flag if the base mark if group area
+ * @param {number} padding padding from the boundary of the chart
+ *
+ * @returns array of 2 bitmaps:
+ *          - first bitmap is filled with all the avoiding marks
+ *          - second bitmap is filled with borders of all the avoiding marks
+ */
 function writeToBitMaps(context, width, height, labelInside, isGroupArea, padding) {
   var layer1 = new BitMap(width, height, padding),
     layer2 = labelInside || isGroupArea ? new BitMap(width, height, padding) : undefined,
@@ -68,6 +114,8 @@ function writeToBitMaps(context, width, height, labelInside, isGroupArea, paddin
     for (y = 0; y < height; y++) {
       for (x = 0; x < width; x++) {
         alpha = canvasBuffer[y * width + x] & ALPHA_MASK;
+        // only fill second layer for group area because labels are only not allowed to place over
+        // border of area
         if (alpha && alpha ^ INSIDE_OPACITY_IN_ALPHA) layer2.mark(x, y);
       }
     }
@@ -76,7 +124,11 @@ function writeToBitMaps(context, width, height, labelInside, isGroupArea, paddin
       for (x = 0; x < width; x++) {
         alpha = canvasBuffer[y * width + x] & ALPHA_MASK;
         if (alpha) {
+          // fill first layer if there is something in canvas in that location
           layer1.mark(x, y);
+
+          // fill second layer if there is a border in canvas in that location
+          // and label can be placed inside
           if (labelInside && alpha ^ INSIDE_OPACITY_IN_ALPHA) layer2.mark(x, y);
         }
       }
@@ -85,6 +137,12 @@ function writeToBitMaps(context, width, height, labelInside, isGroupArea, paddin
   return [layer1, layer2];
 }
 
+/**
+ * Draw mark into canvas
+ * @param {object} context canvas context, to which all avoiding marks are drawn
+ * @param {array} originalItems mark to be drawn into canvas
+ * @param {bool} labelInside a flag if label to be placed inside mark or not
+ */
 function drawMark(context, originalItems, labelInside) {
   var n = originalItems.length;
   if (labelInside) {
@@ -94,9 +152,16 @@ function drawMark(context, originalItems, labelInside) {
     }
   } else items = originalItems;
 
+  // draw items into canvas
   Marks[items[0].mark.marktype].draw(context, { items: items }, null);
 }
 
+/**
+ * draw group of marks into canvas
+ * @param {object} context canvas context, to which all avoiding marks are drawn
+ * @param {array} groups group of marks to be drawn into canvas
+ * @param {bool} labelInside a flag if label to be placed inside mark or not
+ */
 function drawGroup(context, groups, labelInside) {
   var n = groups.length,
     marks;
@@ -105,11 +170,17 @@ function drawGroup(context, groups, labelInside) {
     for (var j = 0; j < marks.length; j++) {
       var g = marks[j];
       if (g.marktype !== 'group') drawMark(context, g.items, labelInside);
-      else drawGroup(context, g.items, labelInside); // nested group might not work.
+      else drawGroup(context, g.items, labelInside); // recursivly draw group of marks
     }
   }
 }
 
+/**
+ * Prepare item before drawing into canvas (setting stroke and opacity)
+ * @param {object} originalItem item to be prepared
+ *
+ * @returns prepared item
+ */
 function prepareMarkItem(originalItem) {
   var item = {};
   for (var key in originalItem) {
