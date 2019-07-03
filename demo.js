fdd30973e80b87c47d4a57c21ea12938330a5c85
Chanwut Kittivorawong <30903997+chanwutk@users.noreply.github.com>
2019-07-02 23:09:32 -0700

    to review (update readme) (#18)

--- demo.js
+++ demo.js
@@ -1,18 +1,42 @@
-vega.transforms.label = Label.label;
+vega.transforms.label = vegaLabel.label;
 
 var jsonText;
 var rawFile = new XMLHttpRequest();
-  rawFile.open('GET', './label_test.vg.json', false);
-  rawFile.onreadystatechange = () => {
-    if(rawFile.readyState === 4)
-      if(rawFile.status === 200 || rawFile.status === 0)
-        jsonText = rawFile.responseText;
-  }
+// prettier-ignore
+var specName
+// ---------area---------
+// = "area_job_voyager";
+// = 'area';
+
+// ---------line---------
+= 'line_connected_scatter';
+// = 'line_end';
+// = 'line';
+
+// ---------rect---------
+// = 'rect_stack';
+// = 'rect';
+
+// -------scatter--------
+// = 'scatter_asteroids_contour';
+// = 'scatter_asteroids';
+// = 'scatter_big_symbol';
+// = 'scatter_car';
+// = 'scatter_test';
+// = 'scatter_zoom';
+
+rawFile.open('GET', '../specs/label_' + specName + '.vg.json', false);
+rawFile.onreadystatechange = () => {
+  if (rawFile.readyState === 4)
+    if (rawFile.status === 200 || rawFile.status === 0) {
+      jsonText = rawFile.responseText;
+    }
+};
 rawFile.send(null);
 
 var spec = JSON.parse(jsonText);
 new vega.View(vega.parse(spec))
-  .renderer('svg')     // set renderer (canvas or svg)
-  .initialize('#vis')  // initialize view within parent DOM container
-  .hover()             // enable hover encode set processing
+  .renderer('canvas') // set renderer (canvas or svg)
+  .initialize('#vis') // initialize view within parent DOM container
+  .hover() // enable hover encode set processing
   .run();
--- src/Label.js
+++ src/Label.js
@@ -1,67 +1,87 @@
-import cloud from './LabelLayout';
+/*eslint no-console: "warn"*/
+import labelLayout from './LabelLayout';
 import {Transform} from 'vega-dataflow';
 import {inherits, isFunction} from 'vega-util';
 
-var Output = ['x', 'y', 'angle', 'fontSize', 'fill'];
+const Output = ['x', 'y', 'opacity', 'align', 'baseline', 'originalOpacity', 'transformed'];
 
-var Params = ['text', 'rotate', 'fontSize'];
+const Params = ['offset'];
+
+const defaultAnchors = ['top-left', 'left', 'bottom-left', 'top', 'bottom', 'top-right', 'right', 'bottom-right'];
 
 export default function Label(params) {
-  Transform.call(this, cloud(), params);
+  Transform.call(this, labelLayout(), params);
 }
 
 Label.Definition = {
-  "type": "Label",
-  "metadata": {"modifies": true},
-  "params": [
-    { "name": "fontSize", "type": "number", "expr": false, "default": 14 },
-    { "name": "rotate", "type": "number", "expr": false, "default": 0 },
-    { "name": "text", "type": "field" },
-    { "name": "as", "type": "string", "array": true, "length": 3, "default": Output }
+  type: 'Label',
+  metadata: {modifies: true},
+  params: [
+    {name: 'padding', type: 'number', default: 0},
+    {name: 'markIndex', type: 'number', default: 0},
+    {name: 'lineAnchor', type: 'string', values: ['begin', 'end'], default: 'end'},
+    {name: 'avoidBaseMark', type: 'boolean', default: true},
+    {name: 'size', type: 'number', array: true, length: [2]},
+    {name: 'offset', type: 'number', default: [1]},
+    {name: 'sort', type: 'field'},
+    {name: 'anchor', type: 'string', default: defaultAnchors},
+    {name: 'avoidMarks', type: 'data', array: true},
+    {
+      name: 'as',
+      type: 'string',
+      array: true,
+      length: Output.length,
+      default: Output
+    }
   ]
 };
 
-var prototype = inherits(Label, Transform);
+const prototype = inherits(Label, Transform);
 
 prototype.transform = function(_, pulse) {
   function modp(param) {
-    var p = _[param];
+    const p = _[param];
     return isFunction(p) && pulse.modified(p.fields);
   }
 
-  var mod = _.modified();
+  const mod = _.modified();
   if (!(mod || pulse.changed(pulse.ADD_REM) || Params.some(modp))) return;
 
-  var data = pulse.materialize(pulse.SOURCE).source,
-      labelLayout = this.value,
-      as = _.as || Output,
-      fontSize = _.fontSize || 14;
-
-  data.forEach(function(t) {
-    t[as[2]] = 0;
-    t[as[3]] = fontSize;
-  });
+  const data = pulse.materialize(pulse.SOURCE).source;
+  const labelLayout = this.value;
+  const as = _.as || Output;
+  const offset = Array.isArray(_.offset) ? _.offset : Number.isFinite(_.offset) ? [_.offset] : [1];
+  const anchor = Array.isArray(_.anchor) ? _.anchor : typeof _.anchor === 'string' ? [_.anchor] : defaultAnchors;
+  const numberPositions = Math.max(offset.length, anchor.length);
 
   // configure layout
-  var labels = labelLayout
-    .points(data)
-    .rotate(_.rotate || 0)
-    .fontSize(fontSize)
+  const labels = labelLayout
+    .texts(data)
+    .sort(_.sort)
+    .offset(offset, numberPositions)
+    .anchor(anchor, numberPositions)
+    .avoidMarks(_.avoidMarks || [])
+    .size(_.size)
+    .avoidBaseMark(_.avoidBaseMark !== undefined ? _.avoidBaseMark : true)
+    .lineAnchor(_.lineAnchor || 'end')
+    .markIndex(_.markIndex || 0)
+    .padding(_.padding || 0)
     .layout();
+  const n = data.length;
 
-  var i = -1,
-      n = labels.length,
-      w, t;
+  // fill the information of transformed labels back into data
+  let l, t;
+  for (let i = 0; i < n; i++) {
+    l = labels[i];
+    t = l.datum;
 
-  while (++i < n) {
-    w = labels[i];
-    t = w.datum;
-
-    t[as[0]] = w.x;
-    t[as[1]] = w.y;
-    t[as[2]] = w.angle;
-    t[as[3]] = w.size;
-    t[as[4]] = w.fill;
+    t[as[0]] = l.x;
+    t[as[1]] = l.y;
+    t[as[2]] = l.opacity;
+    t[as[3]] = l.align;
+    t[as[4]] = l.baseline;
+    t[as[5]] = l.originalOpacity;
+    t[as[6]] = true;
   }
 
   return pulse.reflow(mod).modifies(as);
