ef4f0e8d07202c6d0ab120c24194e4f5e226939d
Chanwut Kittivorawong <chanwutk@gmail.com>
2019-09-28 14:28:05 -0700

    clean up

--- src/OldPixelBasedLabel.js
+++ src/OldPixelBasedLabel.js
@@ -1,12 +1,15 @@
 /*eslint no-unused-vars: "warn"*/
-import { BitMap } from './OldBitMap';
+/*eslint no-console: "warn"*/
+import { BitMap } from "./OldBitMap";
 
 export function placeLabels(data, size, padding) {
-  var textWidth, textHeight,
-      width = 0, height = 0,
-      bitMaps = {},
-      minTextWidth = Number.MAX_SAFE_INTEGER, 
-      minTextHeight = Number.MAX_SAFE_INTEGER;
+  var textWidth,
+    textHeight,
+    width = 0,
+    height = 0,
+    bitMaps = {},
+    minTextWidth = Number.MAX_SAFE_INTEGER,
+    minTextHeight = Number.MAX_SAFE_INTEGER;
 
   data.sort(function(a, b) {
     textWidth = a.textWidth > b.textWidth ? a.textWidth : b.textWidth;
@@ -23,9 +26,9 @@ export function placeLabels(data, size, padding) {
     height = size[1];
   } else {
     data.forEach(function(d) {
-        width = Math.max(width, d.x + d.textWidth);
-        height = Math.max(height, d.y + d.textHeight);
-      });
+      width = Math.max(width, d.x + d.textWidth);
+      height = Math.max(height, d.y + d.textHeight);
+    });
   }
   bitMaps.mark = getMarkBitMap(data, width, height);
   bitMaps.label = new BitMap(width, height);
@@ -34,29 +37,27 @@ export function placeLabels(data, size, padding) {
     bitMaps.mark.unmark(d.x, d.y);
     d.currentPosition = [-1, -1];
     findAvailablePosition(d, bitMaps, padding, function() {
-      if (!checkCollision(d.searchBound, bitMaps.mark)) {
-        d.labelPlaced = true;
-      }
+      return !checkCollision(d.searchBound, bitMaps.mark); 
     });
     bitMaps.mark.mark(d.x, d.y);
 
     if (d.labelPlaced) {
-        minTextWidth = d.textWidth < minTextWidth ? d.textWidth : minTextWidth;
-        minTextHeight = d.textHeight < minTextHeight ? d.textHeight : minTextHeight;
+      minTextWidth = d.textWidth < minTextWidth ? d.textWidth : minTextWidth;
+      minTextHeight =
+        d.textHeight < minTextHeight ? d.textHeight : minTextHeight;
     }
   });
 
   minTextWidth = bitMaps.mark.bin(minTextWidth);
-  minTextHeight = bitMaps.mark.bin(minTextHeight)
+  minTextHeight = bitMaps.mark.bin(minTextHeight);
 
   data.forEach(function(d) {
     d.z = 1;
     if (d.labelPlaced) {
       findAvailablePosition(d, bitMaps, padding, function() {
         d.extendedSearchBound = getExtendedSearchBound(d, bitMaps.mark);
-        if (!checkCollision(d.extendedSearchBound, bitMaps.mark) && !checkCollision(d.searchBound, bitMaps.label)) {
-          d.labelPlaced = true;
-        }
+        return  !checkCollision(d.extendedSearchBound, bitMaps.mark) &&
+          !checkCollision(d.searchBound, bitMaps.label);
       });
 
       if (d.labelPlaced) {
@@ -77,9 +78,10 @@ export function placeLabels(data, size, padding) {
 }
 
 function findAvailablePosition(datum, bitMaps, padding, checkAvailability) {
-  var i, j,
-      searchBound,
-      initJ = datum.currentPosition[1];
+  var i,
+    j,
+    searchBound,
+    initJ = datum.currentPosition[1];
 
   datum.labelPlaced = false;
   for (i = datum.currentPosition[0]; i <= 1 && !datum.labelPlaced; i++) {
@@ -88,14 +90,21 @@ function findAvailablePosition(datum, bitMaps, padding, checkAvailability) {
       datum.boundary = datum.boundaryFun(i, j, padding);
       searchBound = getSearchBound(datum.boundary, bitMaps.mark);
 
-      if (searchBound.startX < 0 || searchBound.startY < 0 || 
-        searchBound.endY >= bitMaps.mark.height || searchBound.endX >= bitMaps.mark.width) {
+      if (
+        searchBound.startX < 0 ||
+        searchBound.startY < 0 ||
+        searchBound.endY >= bitMaps.mark.height ||
+        searchBound.endX >= bitMaps.mark.width
+      ) {
         continue;
       }
-      
+
       datum.currentPosition = [i, j];
       datum.searchBound = searchBound;
-      checkAvailability();
+      if (checkAvailability()) {
+        datum.labelPlaced = true;
+        return;
+      }
     }
     initJ = -1;
   }
@@ -103,14 +112,14 @@ function findAvailablePosition(datum, bitMaps, padding, checkAvailability) {
 
 function getExtendedSearchBound(d, bm) {
   var bound = d.boundary,
-      w = d.textWidth * d.currentPosition[0],
-      h = d.textHeight * d.currentPosition[1];
-    
+    w = d.textWidth * d.currentPosition[0],
+    h = d.textHeight * d.currentPosition[1];
+
   return {
     startX: bm.bin(bound.x + (w < 0 ? w : 0)),
     startY: bm.bin(bound.y + (h < 0 ? h : 0)),
     endX: bm.bin(bound.x2 + (w > 0 ? w : 0)),
-    endY: bm.bin(bound.y2 + (h > 0 ? h : 0)),
+    endY: bm.bin(bound.y2 + (h > 0 ? h : 0))
   };
 }
 
@@ -119,7 +128,7 @@ function getSearchBound(bound, bm) {
     startX: bm.bin(bound.x),
     startY: bm.bin(bound.y),
     endX: bm.bin(bound.x2),
-    endY: bm.bin(bound.y2),
+    endY: bm.bin(bound.y2)
   };
 }
 
@@ -129,18 +138,19 @@ function placeLabel(b, bitMap, minTextWidth, minTextHeight) {
   b.startY = b.startY > 0 ? b.startY : 0;
   b.endX = b.endX < bitMap.width ? b.endX : bitMap.width;
   b.endY = b.endY < bitMap.height ? b.endY : BitMap.height;
-  
-  for (x = b.startX; x < b.endX; x += minTextWidth) {
-    for (y = b.startY; y < b.endY; y += minTextHeight) {
+
+  // look at this one!!!!!!!!
+  for (x = b.startX; x < b.endX; x++) {
+    for (y = b.startY; y < b.endY; y++) {
       bitMap.markBinned(x, y);
     }
   }
 
-  [b.startX, b.endX].forEach(function(_x) {
-    [b.startY, b.endY].forEach(function(_y) {
-      bitMap.markBinned(_x, _y);
-    });
-  });
+  // [b.startX, b.endX].forEach(function(_x) {
+  //   [b.startY, b.endY].forEach(function(_y) {
+  //     bitMap.markBinned(_x, _y);
+  //   });
+  // });
 }
 
 function checkCollision(b, bitMap) {
@@ -157,7 +167,7 @@ function checkCollision(b, bitMap) {
       }
     }
   }
-  
+
   return false;
 }
 
@@ -168,6 +178,6 @@ function getMarkBitMap(data, width, height) {
   data.forEach(function(d) {
     bitMap.mark(d.x, d.y);
   });
-  
+
   return bitMap;
 }
